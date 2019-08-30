2f684e616878e2deb2e4b64a671ade9e828750d6
jaceju <jaceju@kkbox.com>
2019-08-30 10:49:05 +0800

    Add test for a valid expression

--- tests/Scanner.test.js
+++ tests/Scanner.test.js
@@ -28,3 +28,23 @@ test("Scanner should get a numeric token by chars", () => {
   expect(scanner.nextToken()).toEqual(new Token(Tokens.NUMERIC_TOKEN, "456"));
   expect(scanner.nextToken()).toEqual(new Token(Tokens.EOS_TOKEN));
 });
+
+test("Scanner should get tokens from a valid expression", () => {
+  const stream = "25*(3-1)/6+4";
+  const scanner = new Scanner();
+
+  scanner.scan(stream);
+
+  expect(scanner.nextToken()).toEqual(new Token(Tokens.NUMERIC_TOKEN, "25"));
+  expect(scanner.nextToken()).toEqual(new Token(Tokens.MUL_TOKEN, "*"));
+  expect(scanner.nextToken()).toEqual(new Token(Tokens.LEFT_PAREN_TOKEN, "("));
+  expect(scanner.nextToken()).toEqual(new Token(Tokens.NUMERIC_TOKEN, "3"));
+  expect(scanner.nextToken()).toEqual(new Token(Tokens.SUB_TOKEN, "-"));
+  expect(scanner.nextToken()).toEqual(new Token(Tokens.NUMERIC_TOKEN, "1"));
+  expect(scanner.nextToken()).toEqual(new Token(Tokens.RIGHT_PAREN_TOKEN, ")"));
+  expect(scanner.nextToken()).toEqual(new Token(Tokens.DIV_TOKEN, "/"));
+  expect(scanner.nextToken()).toEqual(new Token(Tokens.NUMERIC_TOKEN, "6"));
+  expect(scanner.nextToken()).toEqual(new Token(Tokens.ADD_TOKEN, "+"));
+  expect(scanner.nextToken()).toEqual(new Token(Tokens.NUMERIC_TOKEN, "4"));
+  expect(scanner.nextToken()).toEqual(new Token(Tokens.EOS_TOKEN));
+});
